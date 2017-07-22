6d1f616b014128e7c6b123ee065964b55edc089c
Spyes <lewis@rapidapi.com>
2017-07-22 19:07:27 +0300

    minor syntactic fixes

--- src/Nodes/FunctionNodes/CouchbaseDriver/functions/count.js
+++ src/Nodes/FunctionNodes/CouchbaseDriver/functions/count.js
@@ -6,8 +6,7 @@ module.exports = (bucket, db, { WHERE, KEY }) => {
     const whereStr = WHERE ? `WHERE ${queryString(WHERE)}` : "";
     const keyStr = KEY ? `USE KEYS "${KEY}"` : "";
     const statement = [whereStr, keyStr].reduce((acc, str) => {
-      if (str) acc += ` ${str}`;
-      return acc;
+      return acc += ` ${str || ""}`;
     }, `SELECT COUNT(*) AS COUNT FROM ${bucket}`);
     const query = N1qlQuery.fromString(statement);
     db.query(query, (error, result) => {
